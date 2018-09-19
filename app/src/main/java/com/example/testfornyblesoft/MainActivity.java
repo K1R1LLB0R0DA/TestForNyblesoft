0ad057246584c2cce71647b6a62957fe6fbf4868
kirill <kirillborodavko@gmail.com>
2018-09-19 18:58:17 +0300

    First commit

--- app/src/main/java/com/example/testfornyblesoft/MainActivity.java
+++ app/src/main/java/com/example/testfornyblesoft/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.testfornyblesoft;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        textView = findViewById(R.id.textView);
     }
 }
